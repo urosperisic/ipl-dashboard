7c898e3c9ece82560166ab1b85612660b34aa29b
urosperisic <uros2907@gmail.com>
2021-04-21 23:12:18 +0200

    adding match api and match frontend page six min

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,13 @@
+import {React, useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {MatchDetailCard} from "../components/MatchDetailCard";
+import {MatchSmallCard} from "../components/MatchSmallCard";
+
+export const MatchPage = () => {
+
+    return (
+        <div className="TeamPage">
+            <h1>u</h1>
+        </div>
+    );
+}
