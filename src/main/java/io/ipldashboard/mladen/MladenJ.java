ac064ba6be0c875646474c9cef66e6e453d33b9d
urosperisic <uros2907@gmail.com>
2021-04-25 00:02:41 +0200

    styled the team page

---
+++ src/main/java/io/ipldashboard/mladen/MladenJ.java
@@ -0,0 +1,14 @@
+package io.ipldashboard.mladen;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+// kaggle (mail, same!!!)
+// yarn add node-sass -D
+// npm install react-minimal-pie-chart
+public class MladenJ {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+    }
+}
